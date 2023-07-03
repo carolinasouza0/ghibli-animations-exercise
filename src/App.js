569369e83fbab35dc08d148c36f2590cd51bab80
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-07-03 12:43:10 -0300

    feat: creating the context structure, req 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import { Route, Switch } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
+import FilmsContext from './context/FilmsContext';
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const getFilms = async () => {
+      const response = await fetch('https://api-trybe-frontend.vercel.app/api/ghibli-animations');
+      const data = await response.json();
+      // console.log(data);
+      setFilms(data);
+    };
+    getFilms();
+  }, []);
+
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-      <Route path="/favorites" component={ Favorites } />
-    </Switch>
+    <FilmsContext.Provider value={ { films } }>
+      <Switch>
+        <Route exact path="/" component={ Home } />
+        <Route path="/favorites" component={ Favorites } />
+      </Switch>
+    </FilmsContext.Provider>
   );
 }
 
