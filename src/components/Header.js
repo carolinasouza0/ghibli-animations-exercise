e445c765ebbb0db2b013c4326a4f51f651ed78ec
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-07-03 15:49:03 -0300

    feat: showing the list of films in the Home page, req 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,31 @@
 import { Link, useLocation } from 'react-router-dom';
+import '../styles/Header.css';
+import logo from '../assets/logo.png';
+import heart from '../assets/heart.png';
 
 export default function Header() {
   const { pathname } = useLocation();
   return (
-    <header>
-      <nav>
+    <header className="header-container">
+      <nav className="nav-bar">
+        <img src={ logo } alt="logo" />
         {pathname === '/favorites' ? (
           <Link to="/">Films</Link>
         ) : (
-          <Link to="/favorites">Favorites</Link>
+          <div className="favorites-container">
+            <img
+              src={ heart }
+              alt="heart"
+              className="favorites-logo"
+            />
+            <Link
+              to="/favorites"
+              className="favorites-link"
+            >
+              Favorites
+
+            </Link>
+          </div>
         )}
       </nav>
     </header>
